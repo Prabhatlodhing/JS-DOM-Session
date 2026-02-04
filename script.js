a0fd263e730e0a81cd6d171ef715ac7b813cf3da
Prabhat Lodhi <prabhat@navgurukul.org>
2026-02-04 15:32:11 +0530

    ini comm

---
+++ script.js
@@ -0,0 +1,157 @@
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const heading = document.getElementById('heading');
+
+// heading.style.color = 'blue';
+// heading.style.backgroundColor = 'lightgray';
+// heading.style.padding = '10px';
+// heading.style.textAlign = 'center';
+
+// console.log(heading)
+// const password = document.getElementById('password');
+// const btn = document.getElementById('toggleBtn');
+
+
+// btn.addEventListener('click', ()=>{
+//     if(password.getAttribute('type') === 'password') {
+//         password.setAttribute('type', 'text');
+//         btn.innerText = 'Show Password';
+//     }else{
+//         password.setAttribute('type', 'password');
+//         btn.innerText = 'Hide Password';
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = document.getElementById('name');
+// const btn = document.getElementById('btn');
+
+
+
+// input.addEventListener('input', ()=>{
+//     if(input.value.length > 0 ){
+//         btn.removeAttribute('disabled');
+
+//     }else{
+//         btn.setAttribute('disabled', '');   
+//     }
+// })
+
+
+
+
+
+
+
+
+// const heading = document.querySelector('.container');
+
+
+
+
+
+
+
+// console.log(heading.innerText);
+// console.log(heading.textContent);
+// console.log(heading.innerHTML);
+
+
+
+
+
+
+
+
+
+// const userForm = document.querySelector("#userForm");
+
+
+
+// userForm.addEventListener('submit', (e)=>{
+//     e.preventDefault();
+
+//     const name = document.getElementById("name").value;
+
+
+//     console.log(name);
+
+// });
+
+
+
+
+
+
+
+// const container = document.querySelector('.container');
+
+// container.addEventListener('mouseover', ()=>{
+//     container.style.backgroundColor = 'lightblue';
+// })
+
+// container.addEventListener('mouseout', ()=>{
+//     container.style.backgroundColor = 'red';
+// })
+
+
+
+// const input = document.getElementById('searchBox');
+// const button = document.getElementById('searchBtn');
+
+
+
+
+
+
+
+// input.addEventListener('keydown', (e)=>{
+//     if (e.key === 'Enter') {
+//         alert("The user pressed Enter");
+//     }
+// })
+
+// input.addEventListener('keyup', (e)=>{
+//     if (e.key === 'Enter') {
+//         alert("The user pressed Enter");
+//     }
+// })
+
+
+// input.addEventListener('input', (event)=>{
+//     const value = event.target.value;
+//     console.log("Input event fired", value);
+// })
+
+
+// button.addEventListener('click', ()=>{
+//     console.log("Button clicked");
+// })
